c2c83c530c2631bdceeee28513ee71f91fdc8738
Avi <arindam.avi@gmail.com>
2019-11-10 08:43:15 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import spotipy 
+import spotipy.util as util 
+from json.decoder import JSONDecodeError
+
+
+# MY USER ID: spotify:user:yridp68vzs69q3cn7rdijnyo4
+
+def main():
+    print( __name__)
+
+
+if __name__ == "__main__":
+    main()
