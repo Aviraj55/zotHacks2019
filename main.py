f5b3b0715d7269f4dfd2e50262379a471565933e
Avi <arindam.avi@gmail.com>
2019-11-10 11:31:29 -0800

    try this

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import spotipy 
 import spotipy.util as util 
 from json.decoder import JSONDecodeError
+import spotify_data
 
 
 # MY USER ID: spotify:user:yridp68vzs69q3cn7rdijnyo4
 
-def main():
-    print( __name__)
 
+def main():
+    sp = spotify_data.get_spotify_token()
+    user_recent_tracks = spotify_data.get_recent_tracks(sp)
+    print(user_recent_tracks)
 
 if __name__ == "__main__":
-    main()
+    main()  
