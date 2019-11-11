2f28a2271541450f383cd7c8aa2cac56a1f85e06
Justin Ji <justij4@uci.edu>
2019-11-10 16:19:24 -0800

    Adding Styles

--- src/view/UserInput/UserInput.js
+++ src/view/UserInput/UserInput.js
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import './UserInput.scss';
 import axios from 'axios';
-
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { strict } from "assert";
@@ -30,15 +29,22 @@ function UserInput(props){
         // }).catch(err => {
         //     console.log("Does not work");
         // })
-        fetch('http://localhost:3000/?Id=' + theUser['Id'])
+        fetch('http://localhost:5000/?Id=' + theUser['Id'])
     }
 
     return (
-        <div>
-            <p></p>
-            <Form.Label>Spotify Id</Form.Label>
-            <Form.Control Id = "Id" onChange={handleInput}></Form.Control>
-            <Button onClick={handleSubmit}>Submit</Button>
+        <div className='Admin'>
+            <p className='Topleft'>Zotify</p>
+            <div>
+                <Form.Label>Make Your Spotify Top Hits</Form.Label>
+                <div>
+                    <Form.Control placeholder='Enter Spotify User Id' Id = "Id" onChange={handleInput}></Form.Control>
+                    <div>
+                        <Form.Text></Form.Text>
+                        <Button onClick={handleSubmit}>Create Your Playlist</Button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
